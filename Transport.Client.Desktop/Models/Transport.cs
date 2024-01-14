3dd9290376ec7c28d0bae62fc4015f19bb67e0fd
fiammacrown <admin@ypakala.com>
2024-01-14 17:24:09 +0100

    Update solution structure & add web projects

--- Transport.Client.Desktop/Models/Transport.cs
+++ Transport.Client.Desktop/Models/Transport.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Xml.Linq;
 
 namespace Abeslamidze_Kursovaya7.Models
 {
---
+++ Transport.DTOs/TransportDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Transport.DTOs;
+
+public class TransportDto
+{
+	public Guid Id { get; set; }
+	public double Speed { get; set; }
+	public double Volume { get; set; }
+	public double CurrentLoad { get; set; }
+	public double AvailableVolume { get => Volume - CurrentLoad; set { } }
+	public double PricePerKm { get; set; }
+	//public TransportStatus Status { get; set; }
+}
